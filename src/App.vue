9859afd3ef545de69548398863e2cc85919be984
ksw <yword1@naver.com>
2022-06-02 00:43:30 +0900

    vue

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,157 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
+<div class="header">
+    <ul class="header-button-left">
+      <li @click="step = 0">Cancel</li>
+    </ul>
+    <ul class="header-button-right">
+      <li v-if="step == 1" @click="step = 2">Next</li>
+      <li v-if="step == 2" @click="publish">Commit</li>
+    </ul>
+    <img src="./assets/logo.png" class="logo"/>
+  </div>
 
+  <Container :AppData="AppData" :step="step" :uploadUrl="uploadUrl"/>
+
+  <button @click="more" >더보기</button>
+
+  <div class="footer">
+    <ul class="footer-button-plus">
+      <input @change="upload" accept="image/*" type="file" id="file" class="inputfile" />
+      <label for="file" class="input-plus">+</label>
+    </ul>
+  </div>
+
+
+
+
+</template>
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AppContainer from './components/AppContainer.vue'
+import AppData from './assets/AppData.js'
+import axios from 'axios'
 
 export default {
   name: 'App',
+  data(){
+    return{
+      step : 0,
+      AppData : AppData,
+      moreCount : 0,
+      uploadUrl : Blob,
+    }
+  },
+  methods :{
+    more(){
+      axios.get(`https://codingapple1.github.io/vue/more${this.moreCount}.json`)
+      .then((result)=>{
+        console.log(result.data);
+        this.AppData.push(result.data);
+        this.moreCount++;
+      })
+    },
+    upload(e){
+      let imgFile = e.target.files;
+      console.log(imgFile[0]);
+      this.uploadUrl = URL.createObjectURL(imgFile[0]);
+      this.step = 1;
+    },
+    publish(){
+      var myBoard = {
+        name: "Kim Hyun",
+        userImage: "https://placeimg.com/100/100/arch",
+        postImage: "",
+        likes: 36,
+        date: "May 15",
+        liked: false,
+        content: "오늘 무엇을 했냐면요 아무것도 안했어요 ?",
+        filter: "perpetua"
+      };
+      this.AppData.unshift(myBoard);
+      this.step = 0;
+    }
+  },
   components: {
-    HelloWorld
-  }
+    Container: AppContainer,
+  },
+  
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+body {
+  margin: 0;
+}
+ul {
+  padding: 5px;
+  list-style-type: none;
+}
+.logo {
+  width: 22px;
+  margin: auto;
+  display: block;
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 13px;
+}
+.header {
+  width: 100%;
+  height: 40px;
+  background-color: white;
+  padding-bottom: 8px;
+  position: sticky;
+  top: 0;
+}
+.header-button-left {
+  color: skyblue;
+  float: left;
+  width: 50px;
+  padding-left: 20px;
+  cursor: pointer;
+  margin-top: 10px;
+}
+.header-button-right {
+  color: skyblue;
+  float: right;
+  width: 50px;
+  cursor: pointer;
+  margin-top: 10px;
+}
+.footer {
+  width: 100%;
+  position: sticky;
+  bottom: 0;
+  padding-bottom: 10px;
+  background-color: white;
+}
+.footer-button-plus {
+  width: 80px;
+  margin: auto;
   text-align: center;
-  color: #2c3e50;
+  cursor: pointer;
+  font-size: 24px;
+  padding-top: 12px;
+}
+.sample-box {
+  width: 100%;
+  height: 600px;
+  background-color: bisque;
+}
+.inputfile {
+  display: none;
+}
+.input-plus {
+  cursor: pointer;
+}
+#app {
+  box-sizing: border-box;
+  font-family: "consolas";
   margin-top: 60px;
+  width: 100%;
+  max-width: 460px;
+  margin: auto;
+  position: relative;
+  border-right: 1px solid #eee;
+  border-left: 1px solid #eee;
 }
 </style>
---
+++ src/components/AppPost.vue
@@ -0,0 +1,64 @@
+<template>
+  <div>
+    <div class="post-header">
+      <div class="profile"></div>
+      <span class="profile-name">{{AppData.name}}</span>
+    </div>
+    <div class="post-body" :style="{backgroundImage : `url(${AppData.postImage})`}"></div>
+    <div class="post-content">
+      <p>{{AppData.likes}} Likes</p>
+      <p><strong>{{AppData.name}}</strong> {{AppData.content}}</p>
+      <p class="date">{{AppData.date}}</p>
+    </div>
+</div> 
+</template>
+
+<script>
+export default {
+name: 'AppContainer',
+props : {
+  AppData : Array,
+  },
+}
+</script>
+
+<style>
+.post {
+  width: 100%;
+}
+.profile {
+  background-image: url("https://placeimg.com/100/100/arch");
+  width: 30px;
+  height: 30px;
+  background-size: 100%;
+  border-radius: 50%;
+  float: left;
+}
+.profile-name {
+  display: block;
+  float: left;
+  padding-left: 10px;
+  padding-top: 7px;
+  font-size: 14px;
+}
+.post-header {
+  height: 30px;
+  padding: 10px;
+}
+.post-body {
+  background-image: url("https://placeimg.com/640/480/animals");
+  height: 450px;
+  background-position: center;
+  background-size: cover;
+}
+.post-content {
+  padding-left: 15px;
+  padding-right: 15px;
+  font-size: 14px;
+}
+.date {
+  font-size: 11px;
+  color: grey;
+  margin-top: -8px;
+} 
+</style>
